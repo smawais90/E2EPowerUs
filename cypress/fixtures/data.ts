5fd2454f93482bec5bded2b36adc609e9152ca57
Syed Muhammad Awais <50403616+smawais90@users.noreply.github.com>
2023-07-04 05:29:10 +0200

    courses-test-case

---
+++ cypress/fixtures/data.ts
@@ -0,0 +1,11 @@
+const data= {
+
+    'first-name': 'testFirstName',
+    'last-name': 'testLastName',
+    'email':`test+${Math.random() * 100}@gmail.com`,
+    'password': `${Math.random() * 10}`,
+    'telephone': `176${Math.floor(Math.random() * 100000000)}`,
+}
+
+export default data;
+
---
+++ cypress/fixtures/selectors.ts
@@ -0,0 +1,16 @@
+const selector = {
+    'platform': 'Lernplattform',
+    'RegisterButton': 'Jetzt kostenlos anmelden',
+    'profession': '[title="Elektronik"]',
+    'apprenticeship': '[title="Abgeschlossene Berufsausbildung"]',
+    'first-name': '[autocomplete="given-name"]',
+    'last-name': '[autocomplete="family-name"]',
+    'email': '[autocomplete="email"]',
+    'password': '[type="password"]',
+    'register-button': 'Zum letzten Schritt',
+    'telephone': '[autocomplete="tel"]',
+    'complete_registration': 'Kostenlos registrieren',
+}
+
+export default selector;
+
